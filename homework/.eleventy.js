7609de89bbaa775d73d3ca5b80a42082450fced9
Yoghaswara Hadi Nugroho <56267568+OgataKaede@users.noreply.github.com>
2020-03-02 16:02:14 +0700

    Fix

--- homework/.eleventy.js
+++
@@ -1,7 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('assets')
-  eleventyConfig.addPassthroughCopy('js')
-  return {
-    passthroughFileCopy: true
-  }
-}
--- task/.eleventy.js
+++
@@ -1,8 +0,0 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('assets')
-  eleventyConfig.addPassthroughCopy('js')
-  eleventyConfig.addPassthroughCopy('foto')
-  return {
-    passthroughFileCopy: true
-  }
-}
